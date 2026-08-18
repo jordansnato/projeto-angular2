21ee486ad8fe3b217a4daa59115350a3a96dce7a
jordan <jordansnatos@gmail.com>
2026-08-18 10:10:36 -0300

    estilizar campo de login

--- src/app/feats/execicio5/execicio5.ts
+++ src/app/feats/execicio5/execicio5.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { form, FormField } from "@angular/forms/signals";
+import { form, FormField, required, validate } from "@angular/forms/signals";
 import { InterfaceExercicio5 } from './interface-exercicio5';
 import { InterfaceCadastro } from './interface-cadastro';
 
@@ -11,9 +11,10 @@ import { InterfaceCadastro } from './interface-cadastro';
 })
 export class Execicio5 {
 
+  mensagemSenha = signal('')
   mostrarLogin = signal<InterfaceExercicio5[]>([])
-  loginModel = signal<InterfaceCadastro>({
-     nome:'',
+  loginModel = signal<InterfaceExercicio5>({
+    nome:'',
     email:'',
     senha:'',
     confirmeSenha: '',
@@ -21,11 +22,54 @@ export class Execicio5 {
   });
 
   loginForm = form(this.loginModel, (s)=>{
+    required(s.nome, {message:'Campo nome é obrigatório'});
+    required(s.email, {message:'Campo email é obrigatório'});
+    required(s.senha, {message:'Campo senha é obrigatório'});
+    required(s.confirmeSenha, {message:'Campo confirme a senha é obrigatório'});
+    required(s.nascimento, {message:'Campo nascimento é obrigatório'});
+
+    validate(s.confirmeSenha, ({ value, valueOf }) => {
+
+    if (value() !== valueOf(s.senha)) {
+      return {
+        kind: 'senhasDiferentes',
+        message: 'As senhas devem ser iguais!'
+      };
+    }
+
+    return null;
+  });
 
   })
 
-  cadastro(){
+  cadastro(event:SubmitEvent){
+
+    const user = this.loginModel();
+    event.preventDefault(); 
+
+    if(user.senha !== user.confirmeSenha ){
+      this.mensagemSenha.set('As senhas não são iguais!')
+      return;
+    }
+
+    this.mostrarLogin.update(item =>[...item , user])
+
+    this.loginModel.set({
+      nome:'',
+      email:'',
+      senha:'',
+      confirmeSenha: '',
+      nascimento:null
+    })
+
+    // reseta o touched
+    this.loginForm().reset()
 
   }
 
+  excluir(index:number){
+    this.mostrarLogin.update(usuarios =>
+    usuarios.filter((_, i) => i !== index)
+  );
+  }
 }
--- src/app/feats/execicio5/interface-exercicio5.ts
+++ src/app/feats/execicio5/interface-exercicio5.ts
@@ -2,6 +2,7 @@ export interface InterfaceExercicio5 {
     nome:string;
     email:string;
     senha:string;
+    confirmeSenha:string;
     nascimento:Date |null;
 }
 
